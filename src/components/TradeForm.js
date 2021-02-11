f9ecfdfb21036e5d9db6246793d171764382a5a0
Bjorn Carell <bjorn.carell@tink.com>
2021-02-11 07:28:07 +0100

    TradeForm inputs

--- src/components/TradeForm.js
+++ src/components/TradeForm.js
@@ -1,3 +1,12 @@
 import React from 'react';
 
-export const TradeForm = () => <div>TradeForm</div>;
+export const TradeForm = () => {
+  return (
+    <form>
+      <input data-testid="nameInput" />
+      <input data-testid="quantityInput" />
+      <input data-testid="amountInput" />
+      <button data-testid="submit">Lägg till</button>
+    </form>
+  );
+};
