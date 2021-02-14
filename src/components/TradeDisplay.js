e16b7dd3e5d64f1c7a8ef09d7744070aed9bf1f8
Bjorn Carell <bjorn.carell@tink.com>
2021-02-14 15:44:45 +0100

    feat(TradeDisplay) quantity field

--- src/components/TradeDisplay.js
+++ src/components/TradeDisplay.js
@@ -1,17 +1,29 @@
 import React, {useContext} from 'react';
 import {EquitiesContext} from '../context/equities';
+import {total} from '../calculation-fns/total';
 
 export const TradeDisplay = () => {
   const {equities} = useContext(EquitiesContext);
+  const totalQuantity = total('quantity');
+
   return (
     <>
       <div>Trades</div>
       <table>
         <tbody>
           <tr>
-            {equities &&
-              equities.map(equity => <td key={equity.name}>{equity.name}</td>)}
+            <th>Aktie</th>
+            <th>Antal</th>
+            <th>Belopp</th>
+            <th>GAV</th>
           </tr>
+          {equities &&
+            equities.map(equity => (
+              <tr key={equity.name}>
+                <td>{equity.name}</td>
+                <td data-testid="quantity">{totalQuantity(equity.trades)}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </>
--- src/components/__tests__/TradeDisplay.test.js
+++ src/components/__tests__/TradeDisplay.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import {EquitiesProvider} from '../../context/equities';
 import {TradeDisplay} from '../TradeDisplay';
+import {total} from '../../calculation-fns/total';
 
 describe('TradeDisplay', () => {
   const equities = [
@@ -23,14 +24,30 @@ describe('TradeDisplay', () => {
     },
   ];
 
-  const {queryByText} = render(
-    <EquitiesProvider value={{equities}}>
-      <TradeDisplay />
-    </EquitiesProvider>,
-  );
-
-  it('renders trades on first load', () => {
+  it('renders all names correctly', () => {
+    const {queryByText} = render(
+      <EquitiesProvider value={{equities}}>
+        <TradeDisplay />
+      </EquitiesProvider>,
+    );
     expect(queryByText('Telia')).not.toBeNull();
     expect(queryByText('Ericsson')).not.toBeNull();
+  });
+
+  it('renders all quantity fields correctly', () => {
+    render(
+      <EquitiesProvider value={{equities}}>
+        <TradeDisplay />
+      </EquitiesProvider>,
+    );
+
+    const totalQuantity = total('quantity');
+
+    expect(screen.queryAllByTestId('quantity')[0]).toHaveTextContent(
+      totalQuantity(equities[0].trades),
+    );
+    expect(screen.queryAllByTestId('quantity')[1]).toHaveTextContent(
+      totalQuantity(equities[1].trades),
+    );
   });
 });
