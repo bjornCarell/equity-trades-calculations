f9d566d8a9577a0772e478a8368787072310fdb5
Bjorn Carell <bjorn.carell@tink.com>
2021-02-14 14:50:27 +0100

    refactor(TradeDisplay) move data and render out of it

--- src/components/__tests__/TradeDisplay.test.js
+++ src/components/__tests__/TradeDisplay.test.js
@@ -4,32 +4,32 @@ import {EquitiesProvider} from '../../context/equities';
 import {TradeDisplay} from '../TradeDisplay';
 
 describe('TradeDisplay', () => {
+  const equities = [
+    {
+      name: 'Telia',
+      parentId: 'uuid',
+      trades: [
+        {name: 'Telia', quantity: '150', amount: '5220', id: 1},
+        {name: 'Telia', quantity: '50', amount: '2750', id: 2},
+      ],
+    },
+    {
+      name: 'Ericsson',
+      parentId: 'uuid',
+      trades: [
+        {name: 'Ericsson', quantity: '50', amount: '2220', id: 3},
+        {name: 'Ericsson', quantity: '50', amount: '2750', id: 5},
+      ],
+    },
+  ];
+
+  const {queryByText} = render(
+    <EquitiesProvider value={{equities}}>
+      <TradeDisplay />
+    </EquitiesProvider>,
+  );
+
   it('renders trades on first load', () => {
-    const equities = [
-      {
-        name: 'Telia',
-        parentId: 'uuid',
-        trades: [
-          {name: 'Telia', quantity: '150', amount: '5220', id: 1},
-          {name: 'Telia', quantity: '50', amount: '2750', id: 2},
-        ],
-      },
-      {
-        name: 'Ericsson',
-        parentId: 'uuid',
-        trades: [
-          {name: 'Telia', quantity: '150', amount: '5220', id: 3},
-          {name: 'Telia', quantity: '50', amount: '2750', id: 5},
-        ],
-      },
-    ];
-
-    const {queryByText} = render(
-      <EquitiesProvider value={{equities}}>
-        <TradeDisplay />
-      </EquitiesProvider>,
-    );
-
     expect(queryByText('Telia')).not.toBeNull();
     expect(queryByText('Ericsson')).not.toBeNull();
   });
